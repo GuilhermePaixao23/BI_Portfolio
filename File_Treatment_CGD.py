04c893e9b46f56b56e4b9be8beb141ed15758cd0
GuilhermePaixao23 <paixaocm16@gmail.com>
2024-10-08 21:38:56 +0100

    Add files via upload

---
+++ File_Treatment_CGD.py
@@ -0,0 +1,65 @@
+import os
+import pandas as pd
+
+# Path to the folder containing the CSV files
+folder_path = r"C:\Users\paixa\OneDrive\Documentos\_Training\_Projects\CGD"
+
+# Get a list of all CSV files in the folder
+csv_files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
+
+# Loop through each CSV file
+for file_name in csv_files:
+    file_path = os.path.join(folder_path, file_name)
+    
+    try:
+        # Read the first row of the CSV file
+        first_row = pd.read_csv(file_path, delimiter=';', encoding='latin1', nrows=1, engine='python')
+        
+        # Check if the first cell contains the text "DATE_MOVE"
+        if first_row.columns[0] == "DATE_MOVE":
+            print(f"File {file_name} already processed.")
+        else:
+            print(f"Processing file: {file_name}")
+            
+            # Read CSV file into pandas DataFrame
+            df = pd.read_csv(file_path, delimiter=';', encoding='latin1', skiprows=6, skipfooter=1, engine='python')
+            
+            # Select only columns up to the "H" column
+            df = df.iloc[:, :8]
+
+            # Print the first few rows of the DataFrame
+            print(df.head())
+
+            # Rename columns
+            df = df.rename(columns={df.columns[0]: 'DATE_MOVE', df.columns[1]: 'DATE_VALUE',
+                                    df.columns[2]: 'DSC', df.columns[3]: 'DEBIT', 
+                                    df.columns[4]: 'CREDIT', df.columns[5]: 'ACCOUNTING_BALANCE', 
+                                    df.columns[6]: 'AVAILABLE_BALANCE', df.columns[7]: 'CAT'})
+
+            # Replace "." with "" in the numeric columns
+            numeric_columns = ['DEBIT', 'CREDIT', 'ACCOUNTING_BALANCE', 'AVAILABLE_BALANCE']
+            df[numeric_columns] = df[numeric_columns].replace('\.', '', regex=True)
+
+            print(df.head())
+
+            # Replace NaN values with None
+            df = df.where(pd.notnull(df), None)
+
+            print(df.head())
+
+            # Create a month_key using the first value in the DATE_MOVE column
+            date_sample = df['DATE_MOVE'].iloc[0].strip()  # Retrieve and strip any leading/trailing spaces
+            month_key = date_sample[-4:] + "_" + date_sample[3:5]  # Format as "YYYY_MM"
+
+            new_filename = f"Spendings_{month_key}.csv"
+            new_file_path = os.path.join(folder_path, new_filename)
+            print(new_file_path)
+
+            df.to_csv(new_file_path, index=False, sep=';', encoding='latin1')
+            print(f"File saved as {new_filename}")
+            os.remove(file_path)
+
+    except Exception as e:
+        print(f"Error processing {file_path}: {e}")
+
+print("Files ready for takeoff!")
---
+++ Main_CGD.py
@@ -0,0 +1,79 @@
+import subprocess
+import os
+import pandas as pd
+import pyodbc
+
+# Directory containing CSV files
+directory = r"C:\Users\paixa\OneDrive\Documentos\_Training\_Projects\CGD"
+
+correr_egp = subprocess.run(
+    ["python",r"C:\Users\paixa\OneDrive\Documentos\_Training\_Projects\CGD\PY\File_Treatment_CGD.py"],
+    capture_output=True,
+    text=True,
+    check=True,
+    timeout=10000,
+    )
+
+# SQL Server connection details
+SERVER = 'MSI\SQLEXPRESS'
+DATABASE = 'CONTAS'
+USERNAME = 'guif1410'
+PASSWORD = 'Sql_69@!_GF23'
+
+connection_string = f'Driver={{SQL Server}};' \
+                    f'Server={SERVER};' \
+                    f'Database={DATABASE};' \
+                    f'UID={USERNAME};' \
+                    f'PWD={PASSWORD};'
+
+# Connect to SQL Server
+conn = pyodbc.connect(connection_string)
+cursor = conn.cursor()
+
+# Truncate the table before inserting new data
+cursor.execute("TRUNCATE TABLE [CONTAS].[dbo].[CONTAS_CGD]")
+conn.commit()
+
+# Iterate over CSV files in the directory
+for filename in os.listdir(directory):
+    if filename.endswith(".csv"):
+        filepath = os.path.join(directory, filename)
+        
+        # Read CSV file into pandas DataFrame
+        df = pd.read_csv(filepath, delimiter=';', encoding='latin1')
+
+        #data_cleaning
+
+        df = df.rename(columns={df.columns[0]: 'DATE_MOVE', df.columns[1]: 'DATE_VALUE',df.columns[2]: 'DSC',df.columns[3]: 'DEBIT',df.columns[4]: 'CREDIT',df.columns[5]: 'ACCOUNTING_BALANCE',df.columns[6]: 'AVAILABLE_BALANCE',df.columns[7]: 'CAT'})
+        df['DATE_MOVE'] = pd.to_datetime(df['DATE_MOVE'], dayfirst=True).dt.strftime('%Y%m%d')
+        df['DATE_VALUE'] = pd.to_datetime(df['DATE_VALUE'], dayfirst=True).dt.strftime('%Y%m%d')
+        df = df.where(pd.notnull(df), None)
+
+        Numeric_columns_to_0 = ['DEBIT', 'CREDIT', 'ACCOUNTING_BALANCE', 'AVAILABLE_BALANCE']
+        for col in Numeric_columns_to_0:
+            df[col] = df[col].str.replace(',', '.').astype(float).fillna(0)
+        
+        print(df.head())
+
+        # Insert data into SQL Server table
+        for row in df.itertuples():
+            cursor.execute('''
+                INSERT INTO CONTAS_CGD ([DATE_MOVE], [DATE_VALUE], [DSC], [DEBIT], [CREDIT], [ACCOUNTING_BALANCE], [AVAILABLE_BALANCE], [CAT])
+                VALUES (?,?,?,?,?,?,?,?)
+                ''',
+                row.DATE_MOVE,
+                row.DATE_VALUE,
+                row.DSC,
+                row.DEBIT,
+                row.CREDIT,
+                row.ACCOUNTING_BALANCE,
+                row.AVAILABLE_BALANCE,
+                row.CAT
+            )
+        
+        conn.commit()
+
+# Close connection
+conn.close()
+
+print('success')
